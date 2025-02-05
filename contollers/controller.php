9cebf0b11da17235ed8d6dfe7f0ffc83b30d7df5
David Mantilla <david.mantilla@gopass.com.co>
2025-02-04 21:56:11 -0500

    api

--- contollers/controller.php
+++ contollers/controller.php
@@ -27,7 +27,21 @@ include 'modelo/model.php';
             }
             echo json_encode($pedidos);
         }
+        if($param == '/gestion'){
+            $sql = "SELECT * FROM orden";
+            $result = $conexion->query($sql);
+            $pedidos = [];
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $pedidos[] = $row;
+                }
+            }
+            echo json_encode($pedidos);
+
+        }
     }
+
+
 
     
     
