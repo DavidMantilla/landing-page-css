91b7ad8a87fcb5662d74aa7cd9d6133fe72c50b1
puserfit <amortegui.katherinjuli@gmail.com>
2025-02-18 22:48:39 -0500

    modificacion en el login y soporte y ayuda

--- modelo/model.php
+++ modelo/model.php
@@ -5,7 +5,7 @@ class model {
     public $conexion; // Asegúrate de que esta propiedad esté definida
     private $servername = "localhost";
     private $username = "root";
-    private $password = "9321"; // Asegúrate de que la contraseña sea correcta
+    private $password = ""; // Asegúrate de que la contraseña sea correcta
     private $dbname = "inventariodb";
 
     public function __construct() {
@@ -18,7 +18,6 @@ class model {
         }
     }
 
-    // Agrega métodos adicionales para interactuar con la base de datos
 }
 
 ?>
