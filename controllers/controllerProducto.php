b16c337b5e98414c24035381d18c440a97cc38e6
David Mantilla <david.mantilla@gopass.com.co>
2025-02-19 18:59:16 -0500

    correciones productos

--- controllers/controllerProducto.php
+++ controllers/controllerProducto.php
@@ -12,9 +12,8 @@ class controllerProducto
     {
         $this->model = new model();
         $this->conexion = $this->model->conexion;
-        $this->table="producto";
-        $this->id="id_producto";
-        
+        $this->table = "producto";
+        $this->id = "id_producto";
     }
 
     public function obtener_producto()
@@ -36,14 +35,13 @@ class controllerProducto
     public function buscar_producto($id)
     {
 
-        $sql = "SELECT * FROM $this->table where $this->id=".$id;
+        $sql = "SELECT * FROM $this->table where $this->id=" . $id;
         $result = $this->conexion->query($sql);
-      
+
         $producto = [];
         if ($result->num_rows > 0) {
             $row = $result->fetch_row();
-                $producto = $row;
-            
+            $producto = $row;
         }
         echo json_encode($producto);
     }
@@ -63,53 +61,51 @@ class controllerProducto
     }
 
     public function insertar()
-    {   
-        $imagen_producto=$_FILES["imagen_producto"];
+    {
+        $imagen_producto = $_FILES["imagen_producto"];
         $uploadFileDir = './archivos/';
-        $dest_path = $uploadFileDir .date('YmdHis').$imagen_producto['name'];
-        if( isset($imagen_producto)){
-            print_r($imagen_producto['type']);
-             $tipos=["image/png","image/jpeg","image/svg+xml","image/tiff","image/webp"];
-            if(in_array($imagen_producto['type'],$tipos)){
-                if(!move_uploaded_file($imagen_producto['tmp_name'], $dest_path))
-                {
+        $dest_path = $uploadFileDir . date('YmdHis') . $imagen_producto['name'];
+        if (isset($imagen_producto)) {
+
+            $tipos = ["image/png", "image/jpeg", "image/svg+xml", "image/tiff", "image/webp"];
+            if (in_array($imagen_producto['type'], $tipos)) {
+                if (!move_uploaded_file($imagen_producto['tmp_name'], $dest_path)) {
                     $message = 'hubo un error al subir el archivo';
-                    header('location: /landing%20page%20css/gestiondeproducto.php?error='.$message);
+                    header('location: /landing%20page%20css/gestiondeproducto.php?error=' . $message);
                     exit();
                 }
-                header('location: /landing%20page%20css/gestiondeproducto.php?error='.$message);
-                exit();
+
+                // 
+
+                $existencia = $_REQUEST["existencia"];
+                $descripcion = $_REQUEST["descripcion"];
+                $nombre_producto = $_REQUEST["nombre_producto"];
+                $precio = $_REQUEST["precio"];
+                $categoria = $_REQUEST["categoria"];
+
+                $sql = "INSERT INTO $this->table (imagen_producto,existencia,descripcion,nombre_producto,precio,categoria) values( '$dest_path','$existencia','$descripcion','$nombre_producto','$precio','$categoria')";
+                print($sql);
+                $result = $this->conexion->query($sql);
+                header('location: /landing%20page%20css/gestiondeproducto.php');
+            } else {
+                $message = 'hubo un error al cargar el archivo';
+                header('location: /landing%20page%20css/gestiondeproducto.php?error=' . $message);
             }
         }
-        $existencia=$_REQUEST["existencia"];
-        $descripcion= $_REQUEST["descripcion"];
-        $nombre_producto=$_REQUEST["nombre_producto"];
-        $precio=$_REQUEST["precio"];
-        $categoria=$_REQUEST["categoria"];
-
-        $sql = "INSERT INTO $this->table (imagen_producto,existencia,descripcion,nombre_producto,precio,categoria) values( '$dest_path','$existencia','$descripcion','$nombre_producto','$precio','$categoria')";
- 
-        $result = $this->conexion->query($sql);  
-        header('location: /landing%20page%20css/gestiondeproducto.php');
-        
     }
 
     public function actualizar($id)
-    {   
-        $imagen_producto=$_REQUEST["imagen_producto"];
-        $existencia=$_REQUEST["existencia"];
-        $descripcion= $_REQUEST["descripcion"];
-        $nombre_producto=$_REQUEST["nombre_producto"];
-        $precio=$_REQUEST["precio"];
-        $categoria=$_REQUEST["categoria"];
+    {
+        $imagen_producto = $_REQUEST["imagen_producto"];
+        $existencia = $_REQUEST["existencia"];
+        $descripcion = $_REQUEST["descripcion"];
+        $nombre_producto = $_REQUEST["nombre_producto"];
+        $precio = $_REQUEST["precio"];
+        $categoria = $_REQUEST["categoria"];
 
         $sql = "update $this->table set imagen_producto= '$imagen_producto', existencia='$existencia', descripcion='$descripcion', nombre_producto='$nombre_producto', precio='$precio', categoria='$categoria' where $this->id= $id ";
-        $result = $this->conexion->query($sql);  
-         print($result);
-         echo "soy un mensaje post";
+        $result = $this->conexion->query($sql);
+        print($result);
+        echo "soy un mensaje post";
     }
 };
-
-
-
-?>
